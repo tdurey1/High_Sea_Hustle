b56f16583cf9f75b31c383fdb585ba05391dd7fe
tdurey <tdurey1@harding.edu>
2020-02-08 18:26:00 -0600

    Added Unity Project

---
+++ Quartoo practice/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    GameBoard gameBoard = new GameBoard();
+    List<GamePiece> availablePieces = new List<GamePiece>();
+    Player player1 = new Player();
+    Player player2 = new Player();
+
+    bool exitGame = false;
+
+    void setAvailablePieces ()
+    {
+        availablePieces = gameBoard.getAvailablePieces();
+    }
+
+    void runGame()
+    {
+        while (exitGame == false)
+        {
+            if (!gameBoard.placeGamePiece())
+                continue;
+
+            if (gameBoard.isGameOver())
+            {
+                if (gameBoard.startNewGame())
+                {
+                    gameBoard = new GameBoard();
+                    setAvailablePieces();
+                }
+                else
+                    exitGame = true;
+            }
+            else
+                setAvailablePieces();
+        }
+    }
+}
---
+++ Quartoo practice/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int height = 0;
+    public int emblem = 0;
+    public int color = 0;
+    public int type = 0;
+    public string bitValue = "";
+}
