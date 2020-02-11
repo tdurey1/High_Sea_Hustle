91452efea69aa1b1392172837729161f847ac67a
tdurey <tdurey1@harding.edu>
2020-02-10 20:15:55 -0600

    Added win conditions
    Game knows when a move is a winning move

--- Quartoo practice/Assets/Scripts/GameController.cs
+++ Quartoo practice/Assets/Scripts/GameController.cs
@@ -6,20 +6,24 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public Button[] buttonList;
-    public GamePiece[] gamePieces;
-    public GamePiece[] availablePieces;
+    public List<GamePiece> usedPieces;
+    public List<GamePiece> availablePieces;
     public GamePiece selectedPiece;
     private string playerSide;
     private int moveCount;
 
     void SetGameControllerReferenceOnButtons()
     {
-        for (int i = 0; i < gamePieces.Length; i++)
+        for (int i = 0; i < buttonList.Length; i++)
         {
             buttonList[i].GetComponent<GameBoard>().SetGameControllerReference(this);
-            gamePieces[i].GetComponentInParent<GameBoard>().SetGameControllerReference(this);
             availablePieces[i].GetComponent<GamePiece>().SetGameControllerReference(this);
         }
+    }
+
+    void SetGameControllerReferenceOnUsedPiece()
+    {
+        usedPieces[usedPieces.Count - 1].GetComponent<GamePiece>().SetGameControllerReference(this);
     }
 
     void Awake()
@@ -31,7 +35,7 @@ public class GameController : MonoBehaviour
 
     public GamePiece GetSelectedPiece()
     {
-        return  selectedPiece;
+        return selectedPiece;
     }
 
     public void SetSelectedPiece(GamePiece gamePiece)
@@ -39,21 +43,82 @@ public class GameController : MonoBehaviour
         selectedPiece = gamePiece;
     }
 
+    public void RemoveFromAvailablePieces(GamePiece gamePiece)
+    {
+        availablePieces.Remove(gamePiece);
+    }
+
+    public void AddToUsedPieces(GamePiece gamePiece)
+    {
+        usedPieces.Add(gamePiece);
+        SetGameControllerReferenceOnUsedPiece();
+    }
+
     public void EndTurn()
     {
         moveCount++;
 
-        //if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
-        //{
-        //    GameOver();
-        //}
-
-        if (moveCount >= 16)
-        {
+        // Checks the rows
+        if (CheckWinCondition(buttonList[0], buttonList[1], buttonList[2], buttonList[3]))
             GameOver();
-        }
+        else if (CheckWinCondition(buttonList[4], buttonList[5], buttonList[6], buttonList[7]))
+            GameOver();
+        else if (CheckWinCondition(buttonList[8], buttonList[9], buttonList[10], buttonList[11]))
+            GameOver();
+        else if (CheckWinCondition(buttonList[12], buttonList[13], buttonList[14], buttonList[15]))
+            GameOver();
+
+
+        // Checks the cols
+        if (CheckWinCondition(buttonList[0], buttonList[4], buttonList[8], buttonList[12]))
+            GameOver();
+        else if (CheckWinCondition(buttonList[1], buttonList[5], buttonList[9], buttonList[13]))
+            GameOver();
+        else if (CheckWinCondition(buttonList[2], buttonList[6], buttonList[10], buttonList[14]))
+            GameOver();
+        else if (CheckWinCondition(buttonList[3], buttonList[7], buttonList[11], buttonList[15]))
+            GameOver();
+
+        // Checks the main diagonal (left to right)
+        if (CheckWinCondition(buttonList[0], buttonList[5], buttonList[10], buttonList[15]))
+            GameOver();
+
+        // Checks the secondary diagonal (right to left)
+        if (CheckWinCondition(buttonList[3], buttonList[6], buttonList[9], buttonList[12]))
+            GameOver();
+        // Checks for a tie
+        if (moveCount >= 16)
+            GameOver();
 
         changeSides();
+    }
+
+    bool CheckWinCondition(Button btn1, Button btn2, Button btn3, Button btn4)
+    {
+        GamePiece gamePiece1 = btn1.GetComponentInChildren<GamePiece>();
+        GamePiece gamePiece2 = btn2.GetComponentInChildren<GamePiece>();
+        GamePiece gamePiece3 = btn3.GetComponentInChildren<GamePiece>();
+        GamePiece gamePiece4 = btn4.GetComponentInChildren<GamePiece>();
+
+        // Height will only be either 0 or 1 if a gamePiece was set there; if it is 2 then piece was set by user         
+        if (gamePiece1.height == 2 || gamePiece2.height == 2 || gamePiece3.height == 2 || gamePiece4.height == 2)
+            return false;
+
+        // checks if there are 4 GamePieces next to each other with similiar stats
+        if (gamePiece1.height == gamePiece2.height && gamePiece1.height == gamePiece3.height && gamePiece1.height == gamePiece4.height)
+            return true;
+
+        else if (gamePiece1.color == gamePiece2.color && gamePiece1.color == gamePiece3.color && gamePiece1.color == gamePiece4.color)
+            return true;
+
+        else if (gamePiece1.emblem == gamePiece2.emblem && gamePiece1.emblem == gamePiece3.emblem && gamePiece1.emblem == gamePiece4.emblem)
+            return true;
+
+        else if (gamePiece1.type == gamePiece2.type && gamePiece1.type == gamePiece3.type && gamePiece1.type == gamePiece4.type)
+            return true;
+
+        // if there arent any conditions met, that means that there isn't a winner
+        return false;
     }
 
     void GameOver()
