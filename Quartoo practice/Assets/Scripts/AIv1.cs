d98e6e9fe17881a00a539d426eeeae0ab0593b87
levimason96 <lmason2@harding.edu>
2020-02-12 19:50:23 -0600

    Create AIv1.cs

---
+++ Quartoo practice/Assets/Scripts/AIv1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIv1 : MonoBehaviour
+{
+    //private GameController gameController;
+
+    //Unity's getMove for AI
+    //public void getMove(char[] arr)
+    //{
+    //    List<GamePiece> availablePieces = gameController.availablePieces;
+    //    int numOfAvailablePieces = availablePieces.Count;
+
+    //    System.Random rand = new System.Random();
+    //    int option = rand.Next(numOfAvailablePieces);
+    //    GamePiece chosenMove = availablePieces[option];
+
+    //    gameController.SetSelectedPiece(chosenMove);
+    //}
+
+
+    //Button[] buttonList;
+
+}
