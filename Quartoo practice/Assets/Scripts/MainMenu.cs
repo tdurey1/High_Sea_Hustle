bd591aebd8bd353fa4c949afb9a1b5b7ec5ccabe
Courtney Campbell <hull.e.courtney@gmail.com>
2020-02-16 15:35:56 -0600

    Implement quick game button

---
+++ Quartoo practice/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void quickGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
