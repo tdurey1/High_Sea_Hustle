16bb9b6eafffb6f9c199b7fa3eaa167b2184adc4
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-02-20 22:34:26 -0600

    add help popup prefab (#29)

---
+++ Quartoo practice/Assets/Scripts/HelpMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpMenu : MonoBehaviour
+{
+    public GameObject helpPanel;
+    public void exitButton()
+    {
+        helpPanel.SetActive(false);
+    }
+}
