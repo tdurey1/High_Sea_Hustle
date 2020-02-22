af477721d93430b72e7e8c2c98f5f6d3aeb27a95
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-02-22 12:33:28 -0600

    Add help popup to game screen (#33)
    * style the help prefab
    
    * make text change with images
    
    * add barrel asset to game scene
    
    * add help popup to game scene

---
+++ Quartoo practice/Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showHelpPanel()
+    {
+        helpPanel.SetActive(true);
+        Debug.Log("make panel visible");
+    }
+
+    public void hideHelpPanel()
+    {
+        helpPanel.SetActive(false);
+    }
+}
--- Quartoo practice/Assets/Scripts/HelpMenu.cs
+++ Quartoo practice/Assets/Scripts/HelpMenu.cs
@@ -7,9 +7,19 @@ public class HelpMenu : MonoBehaviour
 {
     public Sprite[] images;
     public Image currentImage;
+    public Text currentText;
     public Button nextImage;
     public Button previousImage;
     public int i = 0;
+
+    private string[] text =
+        {
+            "High Sea Hustle is an adaptation of the modern abstract strategy game Quarto. The game consists of 16 pieces and a board with 16 locations. " +
+            "The pieces have 4 characteristics: gold or silver, tall or short, round or angular, and plain or embossed.",
+            "Players take turns giving their opponent a piece to place, then placing the piece given to them.",
+            "The object of the game is to be the first person to establish a line of 4 pieces which share a common characteristic. " +
+            "The line can be vertical, horizontal, or diagonal.",
+        };
 
     void Start()
     {
@@ -38,5 +48,6 @@ public class HelpMenu : MonoBehaviour
     private void updatePanel()
     {
         currentImage.sprite = images[i];
+        currentText.text = text[i];
     }
 }
