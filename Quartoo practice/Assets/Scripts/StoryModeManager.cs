8c792258f016a5a3b2223a70ba578e32b419a114
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-02-23 20:37:10 -0600

    Add story mode scenes (#41)
    * add initial screen and first image
    
    * add the second screen and add navigation to game screen

---
+++ Quartoo practice/Assets/Scripts/StoryModeManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StoryModeManager : MonoBehaviour
+{
+    public Image currentImage;
+    public Text currentCaption;
+    public Button nextButton;
+    
+    public Sprite[] storyModeScenes;
+    string[] storyModeCaptions = 
+    {
+        "Shipmate: Captain, they’ve beat us again! Those scoundrels. We always arrive just too late...",
+        "Captain: All is not yet lost sailor, prepare to board their ship immediately!"
+    };
+    int i = 0; //which image/scene are we on?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void returnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void nextScreen()
+    {
+        if (i == 0)
+        {
+            Debug.Log("enabling image and caption");
+
+            currentImage.transform.SetAsLastSibling();
+            currentCaption.transform.SetAsLastSibling();
+            nextButton.transform.SetAsLastSibling();
+
+        }
+
+        if (i < storyModeScenes.Length)
+        {
+            currentImage.sprite = storyModeScenes[i];
+            currentCaption.text = storyModeCaptions[i];
+            i++;
+        }
+        else
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
