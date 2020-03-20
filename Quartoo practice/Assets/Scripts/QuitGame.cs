fd84108fa4638707283d900fc252c767e0b6eb37
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-03-20 10:40:24 -0500

    Add quit game button to main menu and game scene (#93)

---
+++ Quartoo practice/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void quitGameClicked()
+    {
+        Application.Quit();
+        Debug.Log("Game is quitting.");
+    }
+
+    //There will need to be cases for the game scene which handle quitting a networked game, etc.
+    //Can be checked with SceneManager.GetActiveScene();
+}
