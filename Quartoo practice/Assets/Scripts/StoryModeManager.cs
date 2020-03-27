a6077cc70c5425cdc481425f612adb57cd27a367
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-03-26 19:22:22 -0500

    Import final assets and Rework story mode (#104)
    * Import final assets and update game logo
    
    * Rework story mode
    
    * Add speech bubbles

--- Quartoo practice/Assets/Scripts/StoryModeManager.cs
+++ Quartoo practice/Assets/Scripts/StoryModeManager.cs
@@ -6,55 +6,60 @@ using UnityEngine.UI;
 
 public class StoryModeManager : MonoBehaviour
 {
-    public Image currentImage;
-    public Text currentCaption;
-    public Button nextButton;
-    
-    public Sprite[] storyModeScenes;
-    string[] storyModeCaptions = 
-    {
-        "Shipmate: Captain, they’ve beat us again! Those scoundrels. We always arrive just too late...",
-        "Captain: All is not yet lost sailor, prepare to board their ship immediately!"
+    public Text currentPirateDialogue;
+    public Text currentSailorDialogue;
+
+    bool isPiratesLine;
+
+    string[] pirateDialogueLines =
+{
+        "Finally! After all this time spent searching the treasure is ours! Hey, what is this navy sailor boy doing on my ship?!",
+        "How do you figure that? We're the ones who tracked it down! Actually, I think I know how we can settle this...",
+        "With a good honest game of Quarto! Whoever wins takes the gold."
+
     };
+
+    string[] sailorDialogueLines =
+    {
+        "I've boarded your ship on behalf of my Captian. That treasure there is ours, it was buried on Royally owned land and we intend to claim it",
+        "How's that?",
+        "Sounds fair enough to me, let's play."
+    };
+
+
     int i = 0; //which image/scene are we on?
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currentPirateDialogue.text = pirateDialogueLines[i];
+        isPiratesLine = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void nextButtonPressed()
     {
-        
-    }
-    public void returnToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
+        if (i < pirateDialogueLines.Length) {
+            if (isPiratesLine) //if its the pirate's turn to talk
+            {
+                currentPirateDialogue.text = pirateDialogueLines[i];
+                isPiratesLine = false;
+            }
+            else //if its the sailor's turn to talk
+            {
+                currentSailorDialogue.text = sailorDialogueLines[i];
+                i++; //only increment every other time so both arrays are accessed at every index
 
-    public void nextScreen()
-    {
-        if (i == 0)
-        {
-            Debug.Log("enabling image and caption");
-
-            currentImage.transform.SetAsLastSibling();
-            currentCaption.transform.SetAsLastSibling();
-            nextButton.transform.SetAsLastSibling();
-
-        }
-
-        if (i < storyModeScenes.Length)
-        {
-            currentImage.sprite = storyModeScenes[i];
-            currentCaption.text = storyModeCaptions[i];
-            i++;
+                isPiratesLine = true;
+            }
         }
         else
         {
             SceneManager.LoadScene("GameScene");
         }
+    }
+
+    public void returnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
