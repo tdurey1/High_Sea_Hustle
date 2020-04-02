45af3074b2169f5bfbb283273ed84cd80cc9f41a
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-04-02 15:03:32 -0500

    Begin player selection panel (#125)
    * Create player selection panel prefab and add avatars
    
    * Add toggle prefab
    
    * Add username input
    
    * Add function headers for username input and toggles
    
    * Add avatar selection handlers and hilight
    
    * Change back button
    
    * Reference script source

---
+++ Quartoo practice/Assets/Scripts/PlayerSelectionPanelController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PlayerSelectionPanelController : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public InputField usernameInput;
+    public Button[] avatarOptions;
+    public GameObject[] avatarHighlights;
+
+    private string username = "Player1";
+    private bool userGoesFirst = true;
+    private bool easyAI = true;
+    private string selectedAvatar = "PirateCaptain";
+
+    public void playButton()
+    {
+        //Not sure if this is the right way, but need some kind of check to provide a default name
+        if (usernameInput.text != "")
+            username = usernameInput.text;
+
+        //Save the username and other relevant data somewhere
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void backButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void firstMoveToggled()
+    {
+        userGoesFirst = !userGoesFirst;
+    }
+
+    public void AIDifficultyToggled()
+    {
+        easyAI = !easyAI;
+    }
+
+    public void avatarButton()
+    {
+        Debug.Log("The name of the button clicked is " + eventSystem.currentSelectedGameObject.name);
+
+        selectedAvatar = eventSystem.currentSelectedGameObject.name;
+
+        activateHighlight(eventSystem.currentSelectedGameObject.name);
+    }
+
+    private void activateHighlight(string avatarName)
+    {
+        if (avatarName == "PirateCaptain")
+            showHilight(0);
+        else if (avatarName == "PirateSailor")
+            showHilight(1);
+        else if (avatarName == "NavyCaptain")
+            showHilight(2);
+        else if (avatarName == "NavySailor")
+            showHilight(3);
+        else
+            return;
+    }
+
+    private void showHilight(int index)
+    {
+        for (int i = 0; i < avatarHighlights.Length; i++)
+        {
+            if (i == index)
+            {
+                avatarHighlights[i].SetActive(true);
+            }
+            else
+            {
+                avatarHighlights[i].SetActive(false);
+            }
+        }
+    }
+
+}
---
+++ Quartoo practice/Assets/Scripts/ToggleController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Adapted from:     https://github.com/Kalxoznik/Unity-Toggle-controller
+public class ToggleController : MonoBehaviour 
+{
+	public  bool isOn;
+
+	public Image toggleBgImage;
+	public RectTransform toggle;
+
+	public GameObject handle;
+	private RectTransform handleTransform;
+
+	private float handleSize;
+	private float onPosX;
+	private float offPosX;
+
+	public float handleOffset;
+
+
+	public float speed;
+	static float t = 0.0f;
+
+	private bool switching = false;
+
+
+	void Awake()
+	{
+		handleTransform = handle.GetComponent<RectTransform>();
+		RectTransform handleRect = handle.GetComponent<RectTransform>();
+		handleSize = handleRect.sizeDelta.x;
+		float toggleSizeX = toggle.sizeDelta.x;
+		onPosX = (toggleSizeX / 2) - (handleSize/2) - handleOffset;
+		offPosX = onPosX * -1;
+
+	}
+
+
+	void Start()
+	{
+		if(isOn)
+		{
+			handleTransform.localPosition = new Vector3(onPosX, 0f, 0f);
+		}
+		else
+		{
+			handleTransform.localPosition = new Vector3(offPosX, 0f, 0f);
+		}
+	}
+		
+	void Update()
+	{
+
+		if(switching)
+		{
+			Toggle(isOn);
+		}
+	}
+
+	public void DoYourStaff()
+	{
+		Debug.Log(isOn);
+	}
+
+	public void Switching()
+	{
+        Debug.Log("Inside Switching()");
+		switching = true;
+	}
+		
+
+
+	public void Toggle(bool toggleStatus)
+	{
+		if(toggleStatus)
+		{
+			handleTransform.localPosition = SmoothMove(handle, onPosX, offPosX);
+		}
+		else 
+		{
+			handleTransform.localPosition = SmoothMove(handle, offPosX, onPosX);
+		}
+			
+	}
+
+
+	Vector3 SmoothMove(GameObject toggleHandle, float startPosX, float endPosX)
+	{
+		
+		Vector3 position = new Vector3 (Mathf.Lerp(startPosX, endPosX, t += speed * Time.deltaTime), 0f, 0f);
+		StopSwitching();
+		return position;
+	}
+
+	Color SmoothColor(Color startCol, Color endCol)
+	{
+		Color resultCol;
+		resultCol = Color.Lerp(startCol, endCol, t += speed * Time.deltaTime);
+		return resultCol;
+	}
+
+	CanvasGroup Transparency (GameObject alphaObj, float startAlpha, float endAlpha)
+	{
+		CanvasGroup alphaVal;
+		alphaVal = alphaObj.gameObject.GetComponent<CanvasGroup>();
+		alphaVal.alpha = Mathf.Lerp(startAlpha, endAlpha, t += speed * Time.deltaTime);
+		return alphaVal;
+	}
+
+	void StopSwitching()
+	{
+		if(t > 1.0f)
+		{
+			switching = false;
+
+			t = 0.0f;
+			switch(isOn)
+			{
+			case true:
+				isOn = false;
+				DoYourStaff();
+				break;
+
+			case false:
+				isOn = true;
+				DoYourStaff();
+				break;
+			}
+
+		}
+	}
+
+}
