e2c56e88c57a7c2188ef1e7de9af34ea324e3d76
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-04-09 17:34:35 -0500

    Add button clicks (#155)
    * Add audio clips and begin adding click effect to prefabs
    
    * Finish adding clicks to all preabs

---
+++ Quartoo practice/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClick : MonoBehaviour
+{
+
+    public AudioClip buttonClick;
+
+    private Button button {  get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    string outputMixer = "Sound Effects";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = buttonClick;
+        source.playOnAwake = false;
+
+        AudioMixer mixer = Resources.Load("MasterMixer") as AudioMixer;
+        source.outputAudioMixerGroup = mixer.FindMatchingGroups("Master/Sound Effects")[0];
+
+        button.onClick.AddListener(() => PlaySound());
+
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(buttonClick);
+    }
+}
