fb878136b96bc6ae23c9bc33d3f559c3a5d59dcc
Courtney Campbell <42685516+crtnye@users.noreply.github.com>
2020-04-10 01:24:31 -0500

    Rearrange game scene (#160)
    * Resize board and game pieces
    
    * Slight repositioning
    
    * Rearrange game scene

---
+++ Quartoo practice/Assets/Scripts/OpponentAvatarBarController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpponentAvatarBarController : MonoBehaviour
+{
+    public GameObject opponentAvatarBar;
+    public Image avatarImage;
+    public Text usernametext;
+
+    public Sprite[] avatarImageOptions;
+
+    //THESE NEED TO BE SET DYNAMICALLY
+    private string opponentsAvatar;
+    private string opponentsUsername;
+    void Start()
+    {
+        if (GameInfo.gameType == 'T') //Tutorial
+            opponentAvatarBar.SetActive(false); //There is no opponent
+        else
+        {
+            opponentAvatarBar.SetActive(true);
+
+            if (GameInfo.gameType == 'H') // Quick game hard
+            {
+                if (GameInfo.avatar == "PirateCaptain")
+                {
+                    //Make opponent navy captain
+                    avatarImage.sprite = avatarImageOptions[2];
+                    usernametext.text = "Navy Captain";
+                }
+                else if (GameInfo.avatar == "NavyCaptain")
+                {
+                    //Make opponent pirate captain
+                    avatarImage.sprite = avatarImageOptions[0];
+                    usernametext.text = "Pirate Captain";
+                }
+                else
+                {
+                    //Default to navy captain
+                    avatarImage.sprite = avatarImageOptions[2];
+                    usernametext.text = "Navy Captain";
+                }
+            }
+            else if (GameInfo.gameType == 'E') //Quick game easy
+            {
+                if (GameInfo.avatar == "PirateSailor")
+                {
+                    //make opponent navy sailor
+                    avatarImage.sprite = avatarImageOptions[3];
+                    usernametext.text = "Navy Sailor";
+                }
+                else if (GameInfo.avatar == "NavySailor")
+                {
+                    //make opponent pirate sailor
+                    avatarImage.sprite = avatarImageOptions[1];
+                    usernametext.text = "Pirate Sailor";
+                }
+                else
+                {
+                    //default to navy sailor
+                    avatarImage.sprite = avatarImageOptions[3];
+                    usernametext.text = "Navy Sailor";
+                }
+            }
+            else if (GameInfo.gameType == 'N') //Networked game
+            {
+                //Access the networked opponent's avatar and username
+            }
+            else if (GameInfo.gameType == 'S') //Story Mode
+            {
+                if (GameInfo.storyModeType == 'E')
+                {
+                    opponentsAvatar = "NavySailor";
+                    usernametext.text = "Navy Sailor";
+                }
+                else if (GameInfo.storyModeType == 'H')
+                {
+                    opponentsAvatar = "NavyCaptain";
+                    usernametext.text = "Navy Captain";
+                }
+                else
+                {
+                    return;
+                }
+                ShowAvatar();
+            }
+            else
+            {
+                //Give the opponent a default avatar and username 
+                opponentsAvatar = "PirateCaptain";
+                usernametext.text = "Opponent Player";
+                ShowAvatar();
+                return;
+            }
+
+            
+        }
+    }
+
+    private void ShowAvatar()
+    {
+        if (GameInfo.avatar == "PirateCaptain")
+        {
+            avatarImage.sprite = avatarImageOptions[0];
+        }
+        else if (GameInfo.avatar == "PirateSailor")
+        {
+            avatarImage.sprite = avatarImageOptions[1];
+        }
+        else if (GameInfo.avatar == "NavyCaptain")
+        {
+            avatarImage.sprite = avatarImageOptions[2];
+        }
+        else if (GameInfo.avatar == "NavySailor")
+        {
+            avatarImage.sprite = avatarImageOptions[3];
+        }
+        else
+        {
+            Debug.Log("Player avatar name was invalid");
+            return;
+        }
+    }
+}
---
+++ Quartoo practice/Assets/Scripts/PlayerAvatarBarController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAvatarBarController : MonoBehaviour
+{
+    public Image avatarImage;
+    public Text usernametext;
+
+    public Sprite[] avatarImageOptions;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //If playing story mode, you're always the pirate
+        if (GameInfo.gameType == 'S')
+        {
+            avatarImage.sprite = avatarImageOptions[0];
+            usernametext.text = "Pirate Captain";
+        }
+        else if (GameInfo.gameType == 'T') //Tutorial
+        {
+            avatarImage.sprite = avatarImageOptions[0];
+            usernametext.text = "New Player";
+        }
+        else
+        {
+            usernametext.text = GameInfo.username;
+            ShowAvatar();
+        }
+    }
+
+    private void ShowAvatar()
+    {
+        if (GameInfo.avatar == "PirateCaptain")
+        {
+            avatarImage.sprite = avatarImageOptions[0];
+        }
+        else if (GameInfo.avatar == "PirateSailor")
+        {
+            avatarImage.sprite = avatarImageOptions[1];
+        }
+        else if (GameInfo.avatar == "NavyCaptain")
+        {
+            avatarImage.sprite = avatarImageOptions[2];
+        }
+        else if (GameInfo.avatar == "NavySailor")
+        {
+            avatarImage.sprite = avatarImageOptions[3];
+        }
+        else
+        {
+            Debug.Log("Player avatar name was invalid");
+            return;
+        }
+    }
+}
